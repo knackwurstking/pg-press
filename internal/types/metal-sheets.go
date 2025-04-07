f6cdb1dbf4b58b57bd687572a644385b5f3fab67
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-07 16:44:09 +0200

    fix: simplify metal sheet data

--- internal/types/metal-sheets.go
+++ internal/types/metal-sheets.go
@@ -3,29 +3,24 @@ package types
 type MetalSheets []MetalSheet
 
 type MetalSheet struct {
-	DBKey DBKey `json:"db_key"`
-
-	Format string           `json:"format"`
-	ToolID string           `json:"tool_id"`
-	Data   []MetalSheetData `json:"data"`
-}
-
-type MetalSheetData struct {
-	Press  Press                 `json:"press"`
-	Filter MetalSheetDataFilter  `json:"filter"`
+	DBKey  DBKey                 `json:"db_key"`
+	Format string                `json:"format"`
+	ToolID string                `json:"tool_id"`
+	Press  MetalSheetPress       `json:"press"`
 	Data   []MetalSheetDataEntry `json:"data"`
 }
 
-type MetalSheetDataFilter []uint8
+type MetalSheetPress struct {
+	DBKey DBKey `json:"db_key"`
+	Nr    Press `json:"nr"`
+}
 
 type MetalSheetDataEntry struct {
-	Thickness float32 `json:"thickness"`
-
-	LowerStampHeight int8 `json:"lower_stamp_height"`
-
-	LowerStampSheets float32 `json:"lower_stamp_sheets"`
-	UpperStampSheets float32 `json:"upper_stamp_sheets"`
-
+	DBKey                  DBKey                  `json:"db_key"`
+	Thickness              float32                `json:"thickness"`
+	LowerStampHeight       int8                   `json:"lower_stamp_height"`
+	LowerStampSheets       float32                `json:"lower_stamp_sheets"`
+	UpperStampSheets       float32                `json:"upper_stamp_sheets"`
 	ThicknessSettings      float32                `json:"thickness_settings"`
 	ThicknessSettingsSacmi ThicknessSettingsSacmi `json:"thickness_settings_sacmi"`
 }
