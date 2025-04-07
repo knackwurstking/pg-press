35b3e7e0453d7aa20e081e36cd16f4ad8a2181cb
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-07 17:15:26 +0200

    feat: add press type constants

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,3 +1,12 @@
 package types
 
+const (
+	PressNone = Press(-1)
+	Press0    = Press(0)
+	Press2    = Press(2)
+	Press3    = Press(3)
+	Press4    = Press(4)
+	Press5    = Press(5)
+)
+
 type Press int8
