b443182b841b0475951cb08a0c062f2fb067cced
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-07 17:34:44 +0200

    feat: implement data for each modification

--- internal/types/metal-sheets.go
+++ internal/types/metal-sheets.go
@@ -3,26 +3,26 @@ package types
 type MetalSheets []MetalSheet
 
 type MetalSheet struct {
-	ModifiedBy []ModifiedBy          `json:"modified_by"`
+	ModifiedBy Modifiers[MetalSheet] `json:"modified_by"`
 	Format     string                `json:"format"`
 	ToolID     string                `json:"tool_id"`
 	Press      MetalSheetPress       `json:"press"`
-	Data       []MetalSheetDataEntry `json:"data"`
+	Data       []MetalSheetData      `json:"data"`
 }
 
 type MetalSheetPress struct {
-	ModifiedBy []ModifiedBy `json:"modified_by"`
-	Nr         Press        `json:"nr"`
+	ModifiedBy Modifiers[MetalSheetPress] `json:"modified_by"`
+	Nr         Press                      `json:"nr"`
 }
 
-type MetalSheetDataEntry struct {
-	ModifiedBy             []ModifiedBy           `json:"modified_by"`
-	Thickness              float32                `json:"thickness"`
-	LowerStampHeight       int8                   `json:"lower_stamp_height"`
-	LowerStampSheets       float32                `json:"lower_stamp_sheets"`
-	UpperStampSheets       float32                `json:"upper_stamp_sheets"`
-	ThicknessSettings      float32                `json:"thickness_settings"`
-	ThicknessSettingsSacmi ThicknessSettingsSacmi `json:"thickness_settings_sacmi"`
+type MetalSheetData struct {
+	ModifiedBy             Modifiers[MetalSheetData] `json:"modified_by"`
+	Thickness              float32                   `json:"thickness"`
+	LowerStampHeight       int8                      `json:"lower_stamp_height"`
+	LowerStampSheets       float32                   `json:"lower_stamp_sheets"`
+	UpperStampSheets       float32                   `json:"upper_stamp_sheets"`
+	ThicknessSettings      float32                   `json:"thickness_settings"`
+	ThicknessSettingsSacmi ThicknessSettingsSacmi    `json:"thickness_settings_sacmi"`
 }
 
 type ThicknessSettingsSacmi struct {
--- internal/types/modified-by.go
+++ internal/types/modified-by.go
@@ -2,20 +2,27 @@ package types
 
 import "fmt"
 
-type ModifiedBy struct {
+type ModifiedByData interface {
+	MetalSheet | MetalSheetPress | MetalSheetData
+}
+
+type ModifiedBy[T ModifiedByData] struct {
 	Time   int    `json:"time"`
 	User   string `json:"user"`
 	UserID int    `json:"user_id"`
+	Data   T      `json:"data"`
 }
 
-func (mb *ModifiedBy) Key() string {
+func (mb *ModifiedBy[T]) Key() string {
 	return fmt.Sprintf("%d:%s:%d", mb.Time, mb.User, mb.UserID)
 }
 
-func (mb *ModifiedBy) Is(k *ModifiedBy) bool {
-	return k.Key() == mb.Key()
+func (mb *ModifiedBy[T]) GetData() *T {
+	return &mb.Data
 }
 
-func (mb *ModifiedBy) IsKey(k string) bool {
-	return k == mb.Key()
+type Modifiers[T ModifiedByData] []ModifiedBy[T]
+
+func (m *Modifiers[T]) Add(mb *ModifiedBy[T]) {
+	*m = append(*m, *mb)
 }
