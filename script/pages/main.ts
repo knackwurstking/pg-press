3e390f8f9ec6b4c0b3adce91d9eadabff5333ccd
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-15 15:19:47 +0200

    chore: convert and clear script/ and public/

--- script/pages/main.ts
+++ script/pages/main.ts
@@ -1,10 +1,1 @@
-import { UIStore } from "../lib/store";
-import { Api } from "../lib/api";
-import * as utils from "../lib/window/utils";
-
-window.store = new UIStore();
-window.api = new Api();
-window.utils = utils.create();
-window.ws = new window.ui.WS<WSMessageData>(
-    (process.env.SERVER_PATH_PREFIX || "") + "/ws",
-);
+// TODO: Initialize global stuff here (`window.*`)
