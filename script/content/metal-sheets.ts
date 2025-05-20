b55fc442beede8d533367455d5674aaa65373950
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-20 06:31:22 +0200

    decode tableSearch query and update location.search on change event

--- script/content/metal-sheets.ts
+++ script/content/metal-sheets.ts
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const tableSearchInput =
         document.querySelector<HTMLInputElement>(`#tableSearch`)!;
 
-    tableSearchInput.value = queryTableSearch();
+    tableSearchInput.value = decodeURIComponent(queryTableSearch());
 
-    //tableSearchInput.oninput = tableSearchInputHandler as (
-    //    ev: Event,
-    //) => Promise<void>;
+    tableSearchInput.onchange = tableSearchInputHandler as (
+        ev: Event,
+    ) => Promise<void>;
 });
 
-//async function tableSearchInputHandler(
-//    ev: Event & { currentTarget: HTMLInputElement },
-//) {
-//    console.debug("search for:", ev.currentTarget.value);
-//}
+async function tableSearchInputHandler(
+    ev: Event & { currentTarget: HTMLInputElement },
+) {
+    console.debug("search for:", ev.currentTarget.value);
+    location.search = "&tableSearch=" + ev.currentTarget.value;
+}
 
 function queryTableSearch(): string {
     return (
