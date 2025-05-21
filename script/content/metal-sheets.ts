d0fa6c69723a0b400a8e1720bae2eed95a74dfa6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-21 10:04:59 +0200

    just add onchange to the html input tag

--- script/content/metal-sheets.ts
+++ script/content/metal-sheets.ts
@@ -1,28 +1,3 @@
-// TODO: create tables
 document.addEventListener("DOMContentLoaded", () => {
-    const tableSearchInput =
-        document.querySelector<HTMLInputElement>(`#tableSearch`)!;
-
-    tableSearchInput.value = decodeURIComponent(queryTableSearch());
-
-    tableSearchInput.onchange = tableSearchInputHandler as (
-        ev: Event,
-    ) => Promise<void>;
+    // ...
 });
-
-async function tableSearchInputHandler(
-    ev: Event & { currentTarget: HTMLInputElement },
-) {
-    console.debug("search for:", ev.currentTarget.value);
-    location.search = "&tableSearch=" + ev.currentTarget.value;
-}
-
-function queryTableSearch(): string {
-    return (
-        location.search
-            .slice(1)
-            .split("&")
-            .find((s) => /tableSearch=/.test(s))
-            ?.split("=", 2)[1] || ""
-    ).replace(/\+/g, " ");
-}
