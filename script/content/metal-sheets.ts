d430e197993e148e5e6494f3e8bb4f42f9c2659a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-22 10:54:31 +0200

    ok, remove metal sheet types (ui), try to handle everything server side

--- script/content/metal-sheets.ts
+++ script/content/metal-sheets.ts
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    // TODO: Update datalist for search inputs
-    // TODO: Request and Render tables
-    // TODO: WebSocket listener for metal sheet table changes
+    // TODO: WebSocket handler
 });
--- script/types.d.ts
+++ script/types.d.ts
@@ -3,26 +3,9 @@ export declare global {
         ui: typeof import("ui");
     }
 
-    interface Component<T = any> {
-        render(target: HTMLElement): Promise<void>;
-        update(data: T): Promise<void>;
-        destroy(): Promise<void>;
-    }
-
-    interface MetalSheetTableCell<T extends string | number | SacmiThickness> {
-        valueType: string;
-        value: T;
-    }
-
-    interface MetalSheetTable {
-        dataSearch: string;
-        head: string[];
-        body: Cell[][];
-        hiddenCells: number[];
-    }
-
-    interface SacmiThickness {
-        current: number;
-        max: number;
-    }
+    //interface Component<T = any> {
+    //    render(target: HTMLElement): Promise<void>;
+    //    update(data: T): Promise<void>;
+    //    destroy(): Promise<void>;
+    //}
 }
