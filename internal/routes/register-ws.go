9451c8902d4339412a235dfdd752d17e778fb4a3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-23 19:14:53 +0200

    websocket handler, register and unregister clients

--- internal/routes/register-ws.go
+++ internal/routes/register-ws.go
@@ -3,17 +3,23 @@ package routes
 
 import (
 	"log/slog"
+	"slices"
+	"sync"
 
 	"github.com/labstack/echo/v4"
 	"golang.org/x/net/websocket"
 )
 
 func RegisterWS(e *echo.Echo, o *Options) {
+	ws := NewWS()
+
 	e.GET(o.ServerPathPrefix+"/htmx/metal-sheets", func(c echo.Context) error {
 		websocket.Handler(func(conn *websocket.Conn) {
 			defer conn.Close()
 
-			// TODO: Register client, need a websocket handler first
+			client := NewWSClient(conn)
+			ws.Register(client)
+			defer ws.Unregister(client)
 
 			for {
 				// TODO: Send initial data, and remove this data from register-frontend
@@ -36,4 +42,57 @@ func RegisterWS(e *echo.Echo, o *Options) {
 
 		return nil
 	})
+}
+
+type WS struct {
+	Clients []*WSClient
+
+	mutex *sync.Mutex
+}
+
+func NewWS() *WS {
+	return &WS{
+		Clients: make([]*WSClient, 0),
+		mutex:   &sync.Mutex{},
+	}
+}
+
+func (ws *WS) Register(client *WSClient) {
+	ws.mutex.Lock()
+	defer ws.mutex.Unlock()
+
+	if !slices.Contains(ws.Clients, client) {
+		ws.Clients = append(ws.Clients, client)
+	}
+}
+
+func (ws *WS) Unregister(client *WSClient) {
+	ws.mutex.Lock()
+	defer ws.mutex.Unlock()
+
+	newClients := make([]*WSClient, 0)
+	for _, c := range ws.Clients {
+		if c != client {
+			newClients = append(newClients, c)
+		}
+	}
+	ws.Clients = newClients
+}
+
+func (ws *WS) Start() {
+	// TODO: ...
+}
+
+func (ws *WS) Stop() {
+	// TODO: ...
+}
+
+type WSClient struct {
+	Conn *websocket.Conn
+}
+
+func NewWSClient(client *websocket.Conn) *WSClient {
+	return &WSClient{
+		Conn: client,
+	}
 }
