2461b88e70b85131d9090bac6275e1aa4d8f3953
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-23 00:28:29 +0200

    rename "users" command to "user" and move "user" commands

---
+++ cmd/pg-vis/command.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"os"
+
+	"github.com/SuperPaintman/nice/cli"
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+func listUserCommand() cli.Command {
+	return cli.Command{
+		Name: "list",
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			customDBPath := cli.String(cmd, "db",
+				cli.WithShort("d"),
+				cli.Optional,
+			)
+
+			return func(cmd *cli.Command) error {
+				db, err := openDB(customDBPath)
+				if err != nil {
+					return err
+				}
+
+				users, err := db.Users.List()
+				if err != nil {
+					return err
+				}
+
+				t := table.NewWriter()
+				t.SetOutputMirror(os.Stdout)
+				t.AppendHeader(table.Row{"#", "User Name"})
+
+				rows := []table.Row{}
+				for _, u := range users {
+					rows = append(rows,
+						table.Row{
+							u.ID, u.UserName, u.TelegramID,
+						},
+					)
+				}
+
+				t.AppendRows(rows)
+				t.SetStyle(table.StyleLight)
+				t.Render()
+
+				return nil
+			}
+		}),
+	}
+}
+
+func showUserCommand() cli.Command {
+	return cli.Command{
+		Name: "show",
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			return func(cmd *cli.Command) error {
+				// TODO: Show user information
+
+				return errUnderConstruction
+			}
+		}),
+	}
+}
+
+func addUserCommand() cli.Command {
+	return cli.Command{
+		Name: "add",
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			return func(cmd *cli.Command) error {
+				// TODO: Add a new user
+
+				return errUnderConstruction
+			}
+		}),
+	}
+}
+
+func removeUserCommand() cli.Command {
+	return cli.Command{
+		Name: "remove",
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			return func(cmd *cli.Command) error {
+				// TODO: Remove user
+
+				return errUnderConstruction
+			}
+		}),
+	}
+}
+
+func modUserCommand() cli.Command {
+	return cli.Command{
+		Name: "mod",
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			return func(cmd *cli.Command) error {
+				// TODO: Modify user
+
+				return errUnderConstruction
+			}
+		}),
+	}
+}
--- cmd/pg-vis/main.go
+++ cmd/pg-vis/main.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 
 	"github.com/SuperPaintman/nice/cli"
-	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 const (
@@ -33,93 +32,14 @@ func main() {
 		Name: appName,
 		Commands: []cli.Command{
 			{
-				Name:  "users",
+				Name:  "user",
 				Usage: cli.Usage("Handle users, add, delete or modify user data in the database"),
 				Commands: []cli.Command{
-					{
-						Name: "list",
-						Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-							customDBPath := cli.String(cmd, "db",
-								cli.WithShort("d"),
-								cli.Optional,
-							)
-
-							return func(cmd *cli.Command) error {
-								db, err := openDB(customDBPath)
-								if err != nil {
-									return err
-								}
-
-								users, err := db.Users.List()
-								if err != nil {
-									return err
-								}
-
-								t := table.NewWriter()
-								t.SetOutputMirror(os.Stdout)
-								t.AppendHeader(table.Row{"#", "User Name"})
-
-								rows := []table.Row{}
-								for _, u := range users {
-									rows = append(rows, 
-										table.Row{
-											u.ID, u.UserName, u.TelegramID,
-										}
-									)
-								}
-
-								t.AppendRows(rows)
-								t.SetStyle(table.StyleLight)
-								t.Render()
-
-								return nil 
-							}
-						}),
-					},
-
-					{
-						Name: "show",
-						Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-							return func(cmd *cli.Command) error {
-								// TODO: Show user information
-
-								return errUnderConstruction
-							}
-						}),
-					},
-
-					{
-						Name: "add",
-						Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-							return func(cmd *cli.Command) error {
-								// TODO: Add a new user
-
-								return errUnderConstruction
-							}
-						}),
-					},
-
-					{
-						Name: "remove",
-						Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-							return func(cmd *cli.Command) error {
-								// TODO: Remove user
-
-								return errUnderConstruction
-							}
-						}),
-					},
-
-					{
-						Name: "mod",
-						Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
-							return func(cmd *cli.Command) error {
-								// TODO: Modify user
-
-								return errUnderConstruction
-							}
-						}),
-					},
+					listUserCommand(),
+					showUserCommand(),
+					addUserCommand(),
+					removeUserCommand(),
+					modUserCommand(),
 				},
 			},
 
