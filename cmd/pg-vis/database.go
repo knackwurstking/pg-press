7b522a3505f11a4f74f198ddd0859502c74470d3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-22 19:43:53 +0200

    open database

--- cmd/pg-vis/database.go
+++ cmd/pg-vis/database.go
@@ -1,3 +1,18 @@
 package main
 
-// TODO: Database stuff here, read and write
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/knackwurstking/pg-vis/pkg/pgvis"
+)
+
+func openDB(path string) (*pgvis.DB, error) {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+
+	return pgvis.NewDB(db), nil
+}
