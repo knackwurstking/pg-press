8e6aa276e3916089f871614faf60c95eb8689edd
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-22 22:07:12 +0200

    check if database path exists

--- cmd/pg-vis/database.go
+++ cmd/pg-vis/database.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 
@@ -9,6 +10,10 @@ import (
 )
 
 func openDB(path string) (*pgvis.DB, error) {
+	if _, err := os.Stat(path); err != nil {
+		return nil, err
+	}
+
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
