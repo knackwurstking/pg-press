9a80b795c2a69ae723defc5205119864b05a1715
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-23 21:03:16 +0200

    create "users" table if not exists

--- pkg/pgvis/database.go
+++ pkg/pgvis/database.go
@@ -1,6 +1,8 @@
 package pgvis
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 // DB contains all database tables
 type DB struct {
@@ -12,6 +14,7 @@ type DB struct {
 func NewDB(db *sql.DB) *DB {
 	return &DB{
 		Users: NewDBUsers(db),
+		db:    db,
 	}
 }
 
@@ -20,9 +23,20 @@ type DBUsers struct {
 }
 
 func NewDBUsers(db *sql.DB) *DBUsers {
-	// TODO: Create users table
+	query := `
+		CREATE TABLE IF NOT EXISTS users (
+			"id" INTEGER NOT NULL,
+			"telegram_id" INTEGER NOT NULL,
+			PRIMARY KEY("id" AUTOINCREMENT, "telegram_id")
+		);
+	`
+	if _, err := db.Exec(query); err != nil {
+		panic(err)
+	}
 
-	return &DBUsers{}
+	return &DBUsers{
+		db: db,
+	}
 }
 
 func (db *DBUsers) List() ([]*User, error) {
