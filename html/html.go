984df2672eef869768e9747dd7619c4c2c512fe6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-30 22:26:35 +0200

    serve pages

--- html/html.go
+++ html/html.go
@@ -2,6 +2,8 @@ package html
 
 import (
 	"embed"
+	"html/template"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -20,19 +22,52 @@ func Serve(e *echo.Echo, options Options) {
 	e.StaticFS(options.ServerPathPrefix+"/", echo.MustSubFS(static, "static"))
 
 	e.GET(options.ServerPathPrefix+"/", func(c echo.Context) error {
-		// TODO: ...
+		t, err := template.ParseFS(pages,
+			"layout.html",
+			"pages/page.html",
+		)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		err = t.Execute(c.Response(), nil)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
 
 		return nil
 	})
 
 	e.GET(options.ServerPathPrefix+"/signup", func(c echo.Context) error {
-		// TODO: ...
+		t, err := template.ParseFS(pages,
+			"layout.html",
+			"pages/signup/page.html",
+		)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		err = t.Execute(c.Response(), nil)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
 
 		return nil
 	})
 
 	e.GET(options.ServerPathPrefix+"/feed", func(c echo.Context) error {
-		// TODO: ...
+		t, err := template.ParseFS(pages,
+			"layout.html",
+			"pages/feed/page.html",
+		)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		err = t.Execute(c.Response(), nil)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
 
 		return nil
 	})
