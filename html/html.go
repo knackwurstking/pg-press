3b27c183d6890f36b4ab8c14292a32fb0ead3380
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-30 21:12:58 +0200

    embed all files

--- html/html.go
+++ html/html.go
@@ -1,3 +1,39 @@
 package html
 
-// TODO: Embed all this shit and serve
+import (
+	"embed"
+
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed pages
+var pages embed.FS
+
+//go:embed static
+var static embed.FS
+
+type Options struct {
+	ServerPathPrefix string
+}
+
+func Serve(e *echo.Echo, options Options) {
+	e.StaticFS(options.ServerPathPrefix+"/", echo.MustSubFS(static, "static"))
+
+	e.GET(options.ServerPathPrefix+"/", func(c echo.Context) error {
+		// TODO: ...
+
+		return nil
+	})
+
+	e.GET(options.ServerPathPrefix+"/signup", func(c echo.Context) error {
+		// TODO: ...
+
+		return nil
+	})
+
+	e.GET(options.ServerPathPrefix+"/feed", func(c echo.Context) error {
+		// TODO: ...
+
+		return nil
+	})
+}
