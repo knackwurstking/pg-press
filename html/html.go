e42d29443d73399cc137e45bb6bcfb0aee298ce3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-01 14:28:59 +0200

    clen up: move route handlers

--- html/html.go
+++ html/html.go
@@ -25,68 +25,80 @@ func Serve(e *echo.Echo, options Options) {
 	e.StaticFS(options.ServerPathPrefix+"/", echo.MustSubFS(static, "static"))
 
 	e.GET(options.ServerPathPrefix+"/", func(c echo.Context) error {
-		t, err := template.ParseFS(routes,
-			"routes/layout.html",
-			"routes/page.html",
-		)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		err = t.Execute(c.Response(), nil)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		return nil
+		return handleHomePage(c)
 	})
 
 	e.GET(options.ServerPathPrefix+"/signup", func(c echo.Context) error {
-		v, err := c.FormParams()
-		apiKey := v.Get("api-key")
-
-		if apiKey != "" {
-			log.Debugf("Form: Api Key: %#v", apiKey)
-
-			// TODO: Get user for this api key
-
-			// TODO: If user exists for this api key, redirect to start page "/"
-		}
-
-		t, err := template.ParseFS(routes,
-			"routes/layout.html",
-			"routes/signup/page.html",
-		)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		err = t.Execute(c.Response(), SignUpData{
-			ApiKey: apiKey,
-		})
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		return nil
+		return handleSignUp(c, options.DB)
 	})
 
 	e.GET(options.ServerPathPrefix+"/feed", func(c echo.Context) error {
-		t, err := template.ParseFS(routes,
-			"routes/layout.html",
-			"routes/feed/page.html",
-		)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		err = t.Execute(c.Response(), nil)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-		}
-
-		return nil
+		return handleFeed(c)
 	})
+}
+
+func handleHomePage(c echo.Context) error {
+	t, err := template.ParseFS(routes,
+		"routes/layout.html",
+		"routes/page.html",
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	err = t.Execute(c.Response(), nil)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return nil
+}
+
+func handleSignUp(c echo.Context, db *pgvis.DB) error {
+	v, err := c.FormParams()
+	apiKey := v.Get("api-key")
+
+	if apiKey != "" {
+		log.Debugf("Form: Api Key: %#v", apiKey)
+
+		// TODO: Get user for this api key
+
+		// TODO: If user exists for this api key, redirect to start page "/"
+	}
+
+	t, err := template.ParseFS(routes,
+		"routes/layout.html",
+		"routes/signup/page.html",
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	err = t.Execute(c.Response(), SignUpData{
+		ApiKey: apiKey,
+	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return nil
+}
+
+func handleFeed(c echo.Context) error {
+	t, err := template.ParseFS(routes,
+		"routes/layout.html",
+		"routes/feed/page.html",
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	err = t.Execute(c.Response(), nil)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return nil
 }
 
 type SignUpData struct {
