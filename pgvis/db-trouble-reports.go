155e7ad30005353d66907e7f751f9d189d3dc651
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-11 17:29:11 +0200

    Add DBTroubleReports struct

--- pgvis/db-trouble-reports.go
+++ pgvis/db-trouble-reports.go
@@ -1,9 +1,12 @@
 package pgvis
 
+import "database/sql"
+
 type TroubleReport struct {
-	Modified *Modified[*TroubleReport] `json:"modified"`
+	ID       int                       `json:"id"`
 	Title    string                    `json:"title"`
 	Content  string                    `json:"content"`
+	Modified *Modified[*TroubleReport] `json:"modified"`
 }
 
 func NewTroubleReport(m *Modified[*TroubleReport], title, content string) *TroubleReport {
@@ -12,4 +15,28 @@ func NewTroubleReport(m *Modified[*TroubleReport], title, content string) *Troub
 	}
 }
 
-// TODO: Create DBTroubleReports
+type DBTroubleReports struct {
+	db *sql.DB
+}
+
+func NewDBTroubleReports(db *sql.DB) *DBTroubleReports {
+	query := `
+		CREATE TABLE IF NOT EXISTS cookies (
+			id INTEGER NOT NULL AUTOINCREMENT,
+			title TEXT NOT NULL,
+			content TEXT NOT NULL,
+			modified BLOB NOT NULL,
+			PRIMARY KEY("id")
+		);
+	`
+
+	if _, err := db.Exec(query); err != nil {
+		panic(err)
+	}
+
+	return &DBTroubleReports{
+		db: db,
+	}
+}
+
+// TODO: Add methods: List, Add, Update, Remove
