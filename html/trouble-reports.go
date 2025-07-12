f629295adb0257aa42c434856d8d1e515605eb19
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-12 03:51:45 +0200

    POST and PUT dialog edit

--- html/trouble-reports.go
+++ html/trouble-reports.go
@@ -16,7 +16,7 @@ import (
 
 func ServeTroubleReports(e *echo.Echo, options Options) {
 	e.GET(options.ServerPathPrefix+"/trouble-reports", func(c echo.Context) error {
-		return handleTroubleReportsPage(c, options.DB)
+		return handleTroubleReportsPage(c)
 	})
 
 	e.GET(options.ServerPathPrefix+"/trouble-reports/dialog-edit", func(c echo.Context) error {
@@ -25,6 +25,10 @@ func ServeTroubleReports(e *echo.Echo, options Options) {
 
 	e.POST(options.ServerPathPrefix+"/trouble-reports/dialog-edit", func(c echo.Context) error {
 		return handleTroubleReportsDialogEditPOST(c, options.DB)
+	})
+
+	e.POST(options.ServerPathPrefix+"/trouble-reports/dialog-edit", func(c echo.Context) error {
+		return handleTroubleReportsDialogEditPUT(c, options.DB)
 	})
 
 	e.GET(options.ServerPathPrefix+"/trouble-reports/data", func(c echo.Context) error {
@@ -36,7 +40,7 @@ func ServeTroubleReports(e *echo.Echo, options Options) {
 	})
 }
 
-func handleTroubleReportsPage(ctx echo.Context, db *pgvis.DB) *echo.HTTPError {
+func handleTroubleReportsPage(ctx echo.Context) *echo.HTTPError {
 	t, err := template.ParseFS(templates,
 		"templates/layout.html",
 		"templates/trouble-reports.html",
@@ -101,7 +105,6 @@ func handleTroubleReportsDialogEditGET(submitted bool, ctx echo.Context, db *pgv
 //
 // QueryParam:
 //   - cancel: "true"
-//   - id: int
 //
 // FormValue:
 //   - title: string
@@ -111,31 +114,15 @@ func handleTroubleReportsDialogEditPOST(ctx echo.Context, db *pgvis.DB) *echo.HT
 		return handleTroubleReportsDialogEditGET(true, ctx, db)
 	}
 
-	title, err := url.QueryUnescape(ctx.FormValue("title"))
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusBadRequest,
-			fmt.Errorf("query unescape \"title\" failed: %s", err.Error()),
-		)
-	}
-	title = strings.Trim(title, " \n\r\t")
-
-	content, err := url.QueryUnescape(ctx.FormValue("content"))
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusBadRequest,
-			fmt.Errorf("query unescape \"content\" failed: %s", err.Error()),
-		)
-	}
-	content = strings.Trim(content, " \n\r\t")
-
 	user, ok := ctx.Get("user").(*pgvis.User)
 	if !ok {
 		return echo.NewHTTPError(http.StatusBadRequest, "cannot get the user from the echos context")
 	}
 
-	modified := pgvis.NewModified[*pgvis.TroubleReport](user, nil)
-	tr := pgvis.NewTroubleReport(modified, title, content)
+	title, content, err := getTitleAndContent(ctx)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "query data: %s", err.Error())
+	}
 
 	if title == "" || content == "" {
 		log.Debugf("Invalid input: title=%#v; content=%#v", title, content)
@@ -145,24 +132,70 @@ func handleTroubleReportsDialogEditPOST(ctx echo.Context, db *pgvis.DB) *echo.HT
 			http.StatusBadRequest,
 			fmt.Errorf("invalid input"),
 		) // NOTE: Just a placeholder
-	} else {
-		if id, err := strconv.Atoi(ctx.QueryParam("id")); err != nil || id <= 0 {
-			log.Debugf("Add new database entry: title=%#v; content=%#v", title, content)
-
-			if err = db.TroubleReports.Add(tr); err != nil {
-				return echo.NewHTTPError(
-					http.StatusInternalServerError,
-					fmt.Errorf("add data: %s", err.Error()),
-				)
-			}
-		} else {
-			log.Debugf("Update database entry with id %d: title=%#v; content=%#v", id, title, content)
-			// TODO: Get old data from the database before write the new one, add this to the modified.DataBefore
-			//tr.Modified.DataBefore
-
-			// TODO: Update data with ID in the database (existing data)
-		}
 	}
+
+	log.Debugf("Add new database entry: title=%#v; content=%#v", title, content)
+
+	modified := pgvis.NewModified[*pgvis.TroubleReport](user, nil)
+	tr := pgvis.NewTroubleReport(modified, title, content)
+
+	if err = db.TroubleReports.Add(tr); err != nil {
+		return echo.NewHTTPError(
+			http.StatusInternalServerError,
+			fmt.Errorf("add data: %s", err.Error()),
+		)
+	}
+
+	return handleTroubleReportsDialogEditGET(true, ctx, db)
+}
+
+// handleTroubleReportsDialogEditPUT will add or update data
+//
+// QueryParam:
+//   - cancel: "true"
+//   - id: int
+//
+// FormValue:
+//   - title: string
+//   - content: multiline-string
+func handleTroubleReportsDialogEditPUT(ctx echo.Context, db *pgvis.DB) *echo.HTTPError {
+	if cancel := ctx.QueryParam("cancel"); cancel == "true" {
+		return handleTroubleReportsDialogEditGET(true, ctx, db)
+	}
+
+	id, err := strconv.Atoi(ctx.QueryParam("id"))
+	if err != nil || id <= 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("invalid or missing id"))
+	}
+
+	user, ok := ctx.Get("user").(*pgvis.User)
+	if !ok {
+		return echo.NewHTTPError(http.StatusBadRequest, "cannot get the user from the echos context")
+	}
+
+	title, content, err := getTitleAndContent(ctx)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "query data: %s", err.Error())
+	}
+
+	if title == "" || content == "" {
+		log.Debugf("Invalid input: title=%#v; content=%#v", title, content)
+
+		// TODO: Invalid Input, set inputs to invalid and continue
+		return echo.NewHTTPError(
+			http.StatusBadRequest,
+			fmt.Errorf("invalid input"),
+		) // NOTE: Just a placeholder
+	}
+
+	modified := pgvis.NewModified[*pgvis.TroubleReport](user, nil)
+	_ = pgvis.NewTroubleReport(modified, title, content)
+
+	log.Debugf("Update database entry with id %d: title=%#v; content=%#v", id, title, content)
+	// TODO: Get old data from the database before write the new one, add this to the modified.DataBefore
+	//tr.Modified.DataBefore
+
+	// TODO: Update data with ID in the database (existing data)
 
 	return handleTroubleReportsDialogEditGET(true, ctx, db)
 }
@@ -211,4 +244,26 @@ func handleTroubleReportsDataDELETE(ctx echo.Context, db *pgvis.DB) *echo.HTTPEr
 	}
 
 	return handleTroubleReportsDataGET(ctx, db)
+}
+
+func getTitleAndContent(ctx echo.Context) (title, content string, err error) {
+	title, err = url.QueryUnescape(ctx.FormValue("title"))
+	if err != nil {
+		return title, content, echo.NewHTTPError(
+			http.StatusBadRequest,
+			fmt.Errorf("query unescape \"title\" failed: %s", err.Error()),
+		)
+	}
+	title = strings.Trim(title, " \n\r\t")
+
+	content, err = url.QueryUnescape(ctx.FormValue("content"))
+	if err != nil {
+		return title, content, echo.NewHTTPError(
+			http.StatusBadRequest,
+			fmt.Errorf("query unescape \"content\" failed: %s", err.Error()),
+		)
+	}
+	content = strings.Trim(content, " \n\r\t")
+
+	return title, content, nil
 }
