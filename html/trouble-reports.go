7c38f488e8fcb204ff1407dd552acb08731bc771
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-12 23:23:22 +0200

    Formatting...

--- html/trouble-reports.go
+++ html/trouble-reports.go
@@ -66,10 +66,10 @@ func ServeTroubleReports(e *echo.Echo, options Options) {
 }
 
 type TRDialogEdit struct {
-	ID        int
-	Submitted bool              // Submitted set to true will close the dialog
-	Title string 
-	Content string 
+	ID                int
+	Submitted         bool // Submitted set to true will close the dialog
+	Title             string
+	Content           string
 	LinkedAttachments []*pgvis.Attachment
 }
 
