415f97732976682dd4e2202bbbf07dcc868b2f69
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-14 07:01:59 +0200

    Handle auto clean for telegram id

--- cmd/pg-vis/commands-cookies.go
+++ cmd/pg-vis/commands-cookies.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/SuperPaintman/nice/cli"
+	"github.com/knackwurstking/pg-vis/pgvis"
 )
 
 func removeCookiesCommand() cli.Command {
@@ -63,16 +65,44 @@ func autoCleanCookiesCommand() cli.Command {
 			)
 
 			return func(cmd *cli.Command) error {
-				_, err := openDB(*customDBPath)
+				db, err := openDB(*customDBPath)
 				if err != nil {
 					return err
 				}
 
+
 				if *telegramID != 0 {
-					// TODO: Auto clean up expired cookies for a specific user
-				} else {
-					// TODO: Auto clean up expired cookies for all users
+					u, err := db.Users.Get(*telegramID)
+					if err != nil {
+						if errors.Is(err, pgvis.ErrNotFound) {
+							os.Exit(exitCodeNotFound)
+						}
+
+						fmt.Fprintf(os.Stderr, "Get user \"%d\" failed: %s", *telegramID, err.Error())
+						os.Exit(exitCodeGeneric)
+					}
+
+					cookies, err := db.Cookies.ListApiKey(u.ApiKey)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "List cookies for user \"%d\" failed: %s", *telegramID, err.Error())
+						os.Exit(exitCodeGeneric)
+					}
+
+					now := time.Now().UnixMilli()
+					for _, cookie := range cookies {
+						if now >= cookie.LastLogin {
+							if err = db.Cookies.Remove(cookie.Value); err != nil {
+								// Print out error and continue
+								fmt.Fprintf(os.Stderr, "Removing cookie with value \"%s\" failed: %s", cookie.Value, err.Error())
+							}
+						}
+					}
+
+					return nil
 				}
+
+				// TODO: Auto clean up expired cookies for all users
+				//db.Cookies.List()
 
 				return errors.New("under construction")
 			}
