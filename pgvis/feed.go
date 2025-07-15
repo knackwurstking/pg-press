ce9e01783b9b0f35112dc01f8e34a013bf9e2d93
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-15 20:01:11 +0200

    Adding helper method to the Feed type

--- pgvis/feed.go
+++ pgvis/feed.go
@@ -1,8 +1,25 @@
 package pgvis
 
+import "encoding/json"
+
 type Feed[T any] struct {
 	ID      int
 	Time    int64 // Time contains an UNIX millisecond timestamp
 	Content string
 	Data    T
+}
+
+func (f *Feed[T]) DataToSQLBlob() []byte {
+	b, err := json.Marshal(f)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func (f *Feed[T]) SQLBlobToData(b []byte) {
+	err := json.Unmarshal(b, f)
+	if err != nil {
+		panic(err)
+	}
 }
