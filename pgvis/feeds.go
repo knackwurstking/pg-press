d4271f766f72d945a8b5a3f9e273146b44d96d87
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-16 17:03:13 +0200

    Add list feeds methods (database)

--- pgvis/feeds.go
+++ pgvis/feeds.go
@@ -1,6 +1,8 @@
 package pgvis
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type Feeds struct {
 	db *sql.DB
@@ -26,4 +28,44 @@ func NewFeeds(db *sql.DB) *Feeds {
 	}
 }
 
-// TODO: List method, allow count (from .. to)
+func (f *Feeds) List() ([]*Feed, error) {
+	r, err := f.db.Query(
+		"SELECT id, time, data FROM feeds ORDER BY id DESC",
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	return f.scanAllRows(r)
+}
+
+func (f *Feeds) ListRange(from int, count int) ([]*Feed, error) {
+	r, err := f.db.Query(
+		"SELECT id, time, data FROM feeds ORDER BY id DESC LIMIT ? OFFSET ?",
+		count, from,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	return f.scanAllRows(r)
+}
+
+func (f *Feeds) scanAllRows(r *sql.Rows) (feeds []*Feed, err error) {
+	feeds = []*Feed{}
+	for r.Next() {
+		f := &Feed{}
+		data := []byte{}
+
+		if err = r.Scan(&f.ID, &f.Time, &data); err != nil {
+			return nil, err
+		}
+
+		f.JSONToData(data)
+		feeds = append(feeds, f)
+	}
+
+	return feeds, nil
+}
