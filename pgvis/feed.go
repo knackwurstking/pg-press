2fd9c8032cefdd48a71dfb216d06b1eb06d74a9b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-17 20:21:13 +0200

    ai: clean up and reorganize code

--- pgvis/feed.go
+++ pgvis/feed.go
@@ -2,16 +2,36 @@ package pgvis
 
 import (
 	"fmt"
+	"html"
 	"time"
 )
 
+const (
+	// HTML templates for different feed types
+	userAddTemplate        = `<p>New user: %s</p>`
+	userRemoveTemplate     = `<p>%s Kicked!</p>`
+	userNameChangeTemplate = `<p>User name changed from %s to %s</p>`
+
+	troubleReportAddTemplate = `<p>New trouble report: #%d</p>
+<p>Last modified by: %s</p>
+<a href="/trouble-reports#feed%d">%s</a>`
+
+	troubleReportRemoveTemplate = `<p>Trouble report #%d removed</p>`
+
+	troubleReportUpdateTemplate = `<p>Trouble report #%d updated</p>
+<p>Last modified by: %s</p>
+<a href="/trouble-reports#feed%d">%s</a>`
+)
+
+// Feed represents a feed entry in the system
 type Feed struct {
-	ID    int
-	Time  int64 // Time contains an UNIX millisecond timestamp
-	Main  string
-	Cache any
+	ID    int    `json:"id"`
+	Time  int64  `json:"time"`  // UNIX millisecond timestamp
+	Main  string `json:"main"`  // HTML content for the feed
+	Cache any    `json:"cache"` // Additional cached data
 }
 
+// NewFeed creates a new feed entry with the current timestamp
 func NewFeed(main string, cache any) *Feed {
 	return &Feed{
 		Time:  time.Now().UnixMilli(),
@@ -20,58 +40,88 @@ func NewFeed(main string, cache any) *Feed {
 	}
 }
 
+// User-related feed creators
+
+// NewUserAddFeed creates a feed entry for when a new user is added
 func NewUserAddFeed(userName string) *Feed {
-	return NewFeed(
-		fmt.Sprintf(`<p>New user: %s</p>`, userName),
-		nil,
-	)
+	escapedUserName := html.EscapeString(userName)
+	main := fmt.Sprintf(userAddTemplate, escapedUserName)
+	return NewFeed(main, nil)
 }
 
+// NewUserRemoveFeed creates a feed entry for when a user is removed
 func NewUserRemoveFeed(userName string) *Feed {
-	return NewFeed(
-		fmt.Sprintf(`<p>%s Kicked!</p>`, userName),
-		nil,
-	)
+	escapedUserName := html.EscapeString(userName)
+	main := fmt.Sprintf(userRemoveTemplate, escapedUserName)
+	return NewFeed(main, nil)
 }
 
-func NewUserNameChangeFeed(old, new string) *Feed {
-	return NewFeed(
-		fmt.Sprintf(`<p>User name changed from %s to %s</p>`, old, new),
-		nil,
-	)
+// NewUserNameChangeFeed creates a feed entry for when a user changes their name
+func NewUserNameChangeFeed(oldName, newName string) *Feed {
+	escapedOldName := html.EscapeString(oldName)
+	escapedNewName := html.EscapeString(newName)
+	main := fmt.Sprintf(userNameChangeTemplate, escapedOldName, escapedNewName)
+	return NewFeed(main, nil)
 }
 
+// Trouble report-related feed creators
+
+// NewTroubleReportAddFeed creates a feed entry for when a new trouble report is added
 func NewTroubleReportAddFeed(report *TroubleReport) *Feed {
-	return NewFeed(
-		fmt.Sprintf(
-			`
-			    <p>New trouble report: #%d</p>
-				<p>Last modified by: %s</p>
-				<a href="/trouble-reports#feed%d">%s</a>
-			`,
-			report.ID, report.Modified.User.UserName, report.ID, report.Title,
-		),
-		nil,
+	if report == nil {
+		return NewFeed("<p>New trouble report added</p>", nil)
+	}
+
+	var modifiedBy string
+	if report.Modified.User != nil {
+		modifiedBy = html.EscapeString(report.Modified.User.UserName)
+	} else {
+		modifiedBy = "Unknown"
+	}
+
+	escapedTitle := html.EscapeString(report.Title)
+	main := fmt.Sprintf(
+		troubleReportAddTemplate,
+		report.ID,
+		modifiedBy,
+		report.ID,
+		escapedTitle,
 	)
+
+	return NewFeed(main, nil)
 }
 
+// NewTroubleReportRemoveFeed creates a feed entry for when a trouble report is removed
 func NewTroubleReportRemoveFeed(report *TroubleReport) *Feed {
-	return NewFeed(
-		fmt.Sprintf(`<p>Trouble report #%d removed</p>`, report.ID),
-		nil,
-	)
+	if report == nil {
+		return NewFeed("<p>Trouble report removed</p>", nil)
+	}
+
+	main := fmt.Sprintf(troubleReportRemoveTemplate, report.ID)
+	return NewFeed(main, nil)
 }
 
+// NewTroubleReportUpdateFeed creates a feed entry for when a trouble report is updated
 func NewTroubleReportUpdateFeed(report *TroubleReport) *Feed {
-	return NewFeed(
-		fmt.Sprintf(
-			`
-			    <p>Trouble report #%d updated</p>
-				<p>Last modified by: %s</p>
-				<a href="/trouble-reports#feed%d">%s</a>
-			`,
-			report.ID, report.Modified.User.UserName, report.ID, report.Title,
-		),
-		nil,
+	if report == nil {
+		return NewFeed("<p>Trouble report updated</p>", nil)
+	}
+
+	var modifiedBy string
+	if report.Modified.User != nil {
+		modifiedBy = html.EscapeString(report.Modified.User.UserName)
+	} else {
+		modifiedBy = "Unknown"
+	}
+
+	escapedTitle := html.EscapeString(report.Title)
+	main := fmt.Sprintf(
+		troubleReportUpdateTemplate,
+		report.ID,
+		modifiedBy,
+		report.ID,
+		escapedTitle,
 	)
+
+	return NewFeed(main, nil)
 }
