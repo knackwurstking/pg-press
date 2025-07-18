cf92e5bee74f4ed93a814f01aa71e0ab9f21f04e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-18 06:18:28 +0200

    Update version to v1.0.0

--- routes/static/service-worker.js
+++ routes/static/service-worker.js
@@ -19,7 +19,7 @@
  */
 
 // Version and cache configuration
-const VERSION = "pgvis-v1.0.0";
+const VERSION = "v1.0.0";
 const CACHE_PREFIX = "pgvis";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${VERSION}`;
 const DYNAMIC_CACHE = `${CACHE_PREFIX}-dynamic-${VERSION}`;
