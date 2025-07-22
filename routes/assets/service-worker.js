623cc3fc067713597607f4c765feec784e489e94
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-22 12:15:36 +0200

    Update ui

--- routes/assets/service-worker.js
+++ routes/assets/service-worker.js
@@ -19,7 +19,7 @@
  */
 
 // Version and cache configuration
-const VERSION = "v0.0.27";
+const VERSION = "v0.0.28";
 const CACHE_PREFIX = "pgvis";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${VERSION}`;
 const DYNAMIC_CACHE = `${CACHE_PREFIX}-dynamic-${VERSION}`;
