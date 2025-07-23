3ae4218a4838f3e2d76f2bad86447cf553a97a79
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-23 07:45:56 +0200

    Undo last commit

--- routes/assets/service-worker.js
+++ routes/assets/service-worker.js
@@ -19,7 +19,7 @@
  */
 
 // Version and cache configuration
-const VERSION = "v0.0.33";
+const VERSION = "v0.0.34";
 const CACHE_PREFIX = "pgvis";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${VERSION}`;
 const DYNAMIC_CACHE = `${CACHE_PREFIX}-dynamic-${VERSION}`;
