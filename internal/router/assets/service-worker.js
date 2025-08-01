4a6ed9166346013b6a9ae3a9fa93d7e0a8cecbff
Udo Bauer <knackwurstking.tux@gmail.com>
2025-08-01 11:31:33 +0200

    refactor: file picker stuff

--- internal/router/assets/service-worker.js
+++ internal/router/assets/service-worker.js
@@ -19,7 +19,7 @@
  */
 
 // Version and cache configuration
-const VERSION = "v0.0.154";
+const VERSION = "v0.0.155";
 const CACHE_PREFIX = "pgvis";
 const STATIC_CACHE = `${CACHE_PREFIX}-static-${VERSION}`;
 const DYNAMIC_CACHE = `${CACHE_PREFIX}-dynamic-${VERSION}`;
