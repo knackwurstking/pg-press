b579c5463a17ad3ffc58b23bbe09c43bff285460
Udo Bauer <knackwurstking.tux@gmail.com>
2025-08-15 18:10:55 +0200

    Add tools database basics

---
+++ internal/database/tool.go
@@ -0,0 +1,7 @@
+package database
+
+// Tool represents a tool in the database.
+type Tool struct {
+	ID int64 `json:"id"`
+	// TODO: Implement the Tool struct.
+}
---
+++ internal/database/tools.go
@@ -0,0 +1,40 @@
+package database
+
+import "database/sql"
+
+const (
+	createToolsTableQuery = `
+		CREATE TABLE IF NOT EXISTS tools (
+			id INTEGER NOT NULL,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+	`
+
+	// TODO: Implement the Tools struct.
+)
+
+// Tools represents a collection of tools in the database.
+type Tools struct {
+	db    *sql.DB
+	feeds *Feeds
+}
+
+func NewTools(db *sql.DB, feeds *Feeds) *Tools {
+	if _, err := db.Exec(createToolsTableQuery); err != nil {
+		panic(
+			NewDatabaseError(
+				"create_table",
+				"tools",
+				"failed to create tools table",
+				err,
+			),
+		)
+	}
+
+	return &Tools{
+		db:    db,
+		feeds: feeds,
+	}
+}
+
+// TODO: Implement the Tools struct.
