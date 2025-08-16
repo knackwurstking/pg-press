2619d13d2668cf424dbd722436731e9df22aa69d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-08-16 22:38:29 +0200

    Update order of methods

--- internal/handler/tools.go
+++ internal/handler/tools.go
@@ -20,8 +20,8 @@ func (h *Tools) RegisterRoutes(e *echo.Echo) {
 
 	e.GET(h.ServerPathPrefix+path, h.handleToolsPage)
 
-	e.GET(h.ServerPathPrefix+path+"/all/:id", h.handleToolsAllPage)
 	e.GET(h.ServerPathPrefix+path+"/active/:press", h.handleToolsActivePage)
+	e.GET(h.ServerPathPrefix+path+"/all/:id", h.handleToolsAllPage)
 
 	htmxTroubleReports := htmxhandler.Tools{Base: h.NewHTMXHandlerBase(path)}
 	htmxTroubleReports.RegisterRoutes(e)
@@ -38,6 +38,21 @@ func (h *Tools) handleToolsPage(c echo.Context) error {
 	if err := page.Render(c.Request().Context(), c.Response()); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError,
 			"failed to render tools page: "+err.Error())
+	}
+	return nil
+}
+
+func (h *Tools) handleToolsActivePage(c echo.Context) error {
+	press, err := utils.ParseInt64Param(c, constants.QueryParamPress)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest,
+			"failed to parse id: "+err.Error())
+	}
+
+	page := pages.ToolsActivePage(press)
+	if err := page.Render(c.Request().Context(), c.Response()); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError,
+			"failed to render tools active page: "+err.Error())
 	}
 	return nil
 }
@@ -59,21 +74,6 @@ func (h *Tools) handleToolsAllPage(c echo.Context) error {
 	if err := page.Render(c.Request().Context(), c.Response()); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError,
 			"failed to render tools all page: "+err.Error())
-	}
-	return nil
-}
-
-func (h *Tools) handleToolsActivePage(c echo.Context) error {
-	press, err := utils.ParseInt64Param(c, constants.QueryParamPress)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest,
-			"failed to parse id: "+err.Error())
-	}
-
-	page := pages.ToolsActivePage(press)
-	if err := page.Render(c.Request().Context(), c.Response()); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError,
-			"failed to render tools active page: "+err.Error())
 	}
 	return nil
 }
