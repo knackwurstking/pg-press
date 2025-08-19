b7cd1357702e932f1bca29102f8c7ee74123cc3d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-08-19 02:28:17 +0200

    Enable tool creation button

--- internal/templates/pages/tools_templ.go
+++ internal/templates/pages/tools_templ.go
@@ -289,7 +289,7 @@ func allTools(tools []*database.ToolWithNotes) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 14, "\" disabled><i class=\"bi bi-plus-lg\"></i> <span>Erstellen</span></a></div><ul hx-get=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 14, "\"><i class=\"bi bi-plus-lg\"></i> <span>Erstellen</span></a></div><ul hx-get=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
