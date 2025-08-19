07b5fedbc62da57692a03d8d2382387f03384125
Udo Bauer <knackwurstking.tux@gmail.com>
2025-08-19 11:24:20 +0200

    Add tool edit dialog component and handler
    The commit includes both a dialog template for editing tools and its handling
    logic.

--- internal/htmxhandler/tools.go
+++ internal/htmxhandler/tools.go
@@ -2,8 +2,12 @@ package htmxhandler
 
 import (
 	"errors"
+	"net/http"
 
+	"github.com/knackwurstking/pgpress/internal/constants"
 	"github.com/knackwurstking/pgpress/internal/database"
+	"github.com/knackwurstking/pgpress/internal/templates/components"
+	"github.com/knackwurstking/pgpress/internal/utils"
 	"github.com/labstack/echo/v4"
 )
 
@@ -22,8 +26,16 @@ func (h *Tools) handleListAll(c echo.Context) error {
 	return errors.New("under construction")
 }
 
+// handleEdit renders a dialog for editing or creating a tool
 func (h *Tools) handleEdit(c echo.Context) error {
-	// TODO: Implement edit handler
+	id, _ := utils.ParseInt64Query(c, constants.QueryParamID)
 
-	return errors.New("under construction")
+	toolEdit := components.ToolEditDialog(&components.ToolEditDialogProps{
+		ID: id,
+	})
+	if err := toolEdit.Render(c.Request().Context(), c.Response()); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError,
+			"failed to render tool edit dialog: "+err.Error())
+	}
+	return nil
 }
--- internal/templates/components/tools-edit_templ.go
+++ internal/templates/components/tools-edit_templ.go
@@ -8,8 +8,12 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+type ToolEditDialogProps struct {
+	ID int64
+}
+
 // TODO: Dialog for adding or updating a tool
-func ToolsEdit() templ.Component {
+func ToolEditDialog(props *ToolEditDialogProps) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -30,6 +34,25 @@ func ToolsEdit() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<dialog name=\"tool-edit-dialog\" class=\"fullscreen clean\"><form onsubmit=\"event.preventDefault(); this.closest('dialog').close()\"><footer class=\"flex gap justify-end\"><button type=\"button\" class=\"secondary flex gap\" onclick=\"this.closest('dialog').close()\"><i class=\"bi bi-x-circle\"></i> Schließen</button> <button type=\"submit flex gap\"><i class=\"bi bi-check-circle\"></i> ")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if props.ID > 0 {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "Aktualisieren")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		} else {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "Erstellen")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</button></footer></form><script>\n\t\t\tdocument.querySelector(`dialog[name=\"tool-edit-dialog\"]`).showModal();\n\t\t</script></dialog>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		return nil
 	})
 }
