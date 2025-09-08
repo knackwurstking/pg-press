f54e866f34dc8165ab743d57a59abe5eff6cd267
Udo Bauer <knackwurstking.tux@gmail.com>
2025-09-08 08:26:30 +0200

    refactor: udpate note models package

--- internal/database/models/note/note.go
+++ internal/database/models/note/note.go
@@ -3,21 +3,41 @@ package note
 import "time"
 
 const (
-	INFO NoteLevel = iota
+	INFO Level = iota
 	ATTENTION
 	BROKEN
 )
 
-type NoteLevel int
+type Level int
 
 type Note struct {
 	ID        int64     `json:"id"`
-	Level     NoteLevel `json:"level"`
+	Level     Level     `json:"level"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
+func New(l Level, message string) *Note {
+	return &Note{
+		Level:     l,
+		Content:   message,
+		CreatedAt: time.Now(),
+	}
+}
+
 // Important returns true if the note level is ATTENTION or BROKEN
-func (n *Note) Important() bool {
+func (n *Note) IsImportant() bool {
 	return n.Level == ATTENTION || n.Level == BROKEN
+}
+
+func (n *Note) IsInfo() bool {
+	return n.Level == INFO
+}
+
+func (n *Note) IsAttention() bool {
+	return n.Level == ATTENTION
+}
+
+func (n *Note) IsBroken() bool {
+	return n.Level == BROKEN
 }
