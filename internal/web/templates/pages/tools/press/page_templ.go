065a0b2557fc5383da0114c0af7cde1e308efdc3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-09-09 17:22:00 +0200

    Remove unused column in press tool page
    - Updated template to remove "Presse" column from table and corresponding code.

--- internal/web/templates/pages/tools/press/page_templ.go
+++ internal/web/templates/pages/tools/press/page_templ.go
@@ -53,7 +53,7 @@ func Page(props PageProps) templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<main class=\"container fluid\"><div class=\"card warning\"><div class=\"card-body\"><span>In Bearbeitung</span></div></div><!-- Press Cycles Table --><section id=\"cycle-table-section\" class=\"mt-4\"><h5>Pressennutzungsverlauf</h5><figure class=\"w-full overflow-x-scroll\"><table name=\"additional-cycles-table\" class=\"table borderless compact\"><thead><tr><th>ID</th><th>Presse</th><th>Datum</th><th>Werkzeug</th><th>Position</th><th>Gesamtzyklen</th><th>Teilzyklen (berechnet)</th><th></th></tr></thead><!-- TODO: Render rows next --><tbody></tbody></table></figure></section></main>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<main class=\"container fluid\"><div class=\"card warning\"><div class=\"card-body\"><span>In Bearbeitung</span></div></div><!-- Press Cycles Table --><section id=\"cycle-table-section\" class=\"mt-4\"><h5>Pressennutzungsverlauf</h5><figure class=\"w-full overflow-x-scroll\"><table name=\"additional-cycles-table\" class=\"table borderless compact\"><thead><tr><th>ID</th><th>Datum</th><th>Werkzeug</th><th>Position</th><th>Gesamtzyklen</th><th>Teilzyklen (berechnet)</th><th></th></tr></thead><!-- TODO: Render rows next --><tbody></tbody></table></figure></section></main>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
