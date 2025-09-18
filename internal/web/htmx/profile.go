1617b6cbd71047969add53792be27f32bf4702e6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-09-18 23:41:53 +0200

    Refactor `profile.go` and `router.go` to use new `BaseHandler` structure
    - Update `Profile` struct to embed `BaseHandler` - Create factory function
    `NewProfile` for `Profile` - Refactor route handlers to use methods from
    `BaseHandler` - Remove old HTMX handler routes and mark TODOs

--- internal/web/htmx/profile.go
+++ internal/web/htmx/profile.go
@@ -5,66 +5,72 @@ import (
 
 	"github.com/knackwurstking/pgpress/internal/database"
 	"github.com/knackwurstking/pgpress/internal/logger"
+	"github.com/knackwurstking/pgpress/internal/web/handlers"
 	"github.com/knackwurstking/pgpress/internal/web/helpers"
 	"github.com/knackwurstking/pgpress/internal/web/templates/profilepage"
 	"github.com/knackwurstking/pgpress/pkg/models"
-	"github.com/knackwurstking/pgpress/pkg/utils"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Profile struct {
-	DB *database.DB
+	*handlers.BaseHandler
+}
+
+func NewProfile(db *database.DB) *Profile {
+	return &Profile{
+		BaseHandler: handlers.NewBaseHandler(db, logger.HTMXHandlerProfile()),
+	}
 }
 
 func (h *Profile) RegisterRoutes(e *echo.Echo) {
 	helpers.RegisterEchoRoutes(
 		e,
 		[]*helpers.EchoRoute{
-			helpers.NewEchoRoute(http.MethodGet, "/htmx/profile/cookies", h.handleGetCookies),
-			helpers.NewEchoRoute(http.MethodDelete, "/htmx/profile/cookies", h.handleDeleteCookies),
+			helpers.NewEchoRoute(http.MethodGet, "/htmx/profile/cookies",
+				h.HandleGetCookies),
+			helpers.NewEchoRoute(http.MethodDelete, "/htmx/profile/cookies",
+				h.HandleDeleteCookies),
 		},
 	)
 }
 
-func (h *Profile) handleGetCookies(c echo.Context) error {
-	user, err := helpers.GetUserFromContext(c)
+func (h *Profile) HandleGetCookies(c echo.Context) error {
+	user, err := h.GetUserFromContext(c)
 	if err != nil {
-		return err
+		return h.HandleError(c, err, "failed to get user from context")
 	}
 
-	logger.HTMXHandlerProfile().Debug("Fetching cookies for user %s", user.Name)
+	h.LogDebug("Fetching cookies for user %s", user.Name)
 
 	cookies, err := h.DB.Cookies.ListApiKey(user.ApiKey)
 	if err != nil {
-		return echo.NewHTTPError(utils.GetHTTPStatusCode(err),
-			"failed to list cookies: "+err.Error())
+		return h.HandleError(c, err, "failed to list cookies: "+err.Error())
 	}
 
-	logger.HTMXHandlerProfile().Debug("Found %d cookies for user %s", len(cookies), user.Name)
+	h.LogDebug("Found %d cookies for user %s", len(cookies), user.Name)
 
 	cookiesTable := profilepage.Cookies(models.SortCookies(cookies))
-	if err := cookiesTable.Render(c.Request().Context(), c.Response()); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError,
+	err = cookiesTable.Render(c.Request().Context(), c.Response())
+	if err != nil {
+		return h.RenderInternalError(c,
 			"failed to render cookies table: "+err.Error())
 	}
 
 	return nil
 }
 
-func (h *Profile) handleDeleteCookies(c echo.Context) error {
-	value := helpers.SanitizeInput(c.QueryParam("value"))
-	if value == "" {
-		return echo.NewHTTPError(http.StatusBadRequest,
-			"cookie value parameter is required")
+func (h *Profile) HandleDeleteCookies(c echo.Context) error {
+	value, err := h.ParseStringQuery(c, "value")
+	if err != nil {
+		return h.RenderBadRequest(c, err.Error())
 	}
 
-	logger.HTMXHandlerProfile().Info("Deleting cookie with value: %s", value)
+	h.LogInfo("Deleting cookie with value: %s", value)
 
 	if err := h.DB.Cookies.Remove(value); err != nil {
-		return echo.NewHTTPError(utils.GetHTTPStatusCode(err),
-			"failed to delete cookie: "+err.Error())
+		return h.HandleError(c, err, "failed to delete cookie")
 	}
 
-	return h.handleGetCookies(c)
+	return h.HandleGetCookies(c)
 }
