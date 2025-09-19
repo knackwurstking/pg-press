155b4afd3fefadca647d97dbf411c94e97147786
Udo Bauer <knackwurstking.tux@gmail.com>
2025-09-19 07:32:18 +0200

    Removed the unused `ToolEditFormData`

--- internal/web/htmx/form-data.go
+++ internal/web/htmx/form-data.go
@@ -6,14 +6,6 @@ import (
 	"github.com/knackwurstking/pgpress/pkg/models"
 )
 
-type ToolEditFormData struct {
-	Position models.Position     // Position form field name "position"
-	Format   models.Format       // Format form field names "width" and "height"
-	Type     string              // Type form field name "type"
-	Code     string              // Code form field name "code"
-	Press    *models.PressNumber // Press form field name "press-selection"
-}
-
 type CycleEditFormData struct {
 	TotalCycles  int64 // TotalCycles form field name "total_cycles"
 	PressNumber  *models.PressNumber
