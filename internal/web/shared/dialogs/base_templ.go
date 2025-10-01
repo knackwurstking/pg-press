be1eea4ba1c2b3bdd4389e8ecc9746a553db673a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-01 02:07:14 +0200

    Add padding to dialog form to fix hidden focus outline on submit button

--- internal/web/shared/dialogs/base_templ.go
+++ internal/web/shared/dialogs/base_templ.go
@@ -113,7 +113,7 @@ func Base(props BaseProps) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "><form class=\"flex flex-col gap\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "><form class=\"flex flex-col gap p\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
