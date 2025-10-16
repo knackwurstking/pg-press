355ba6019db2aa1eec71e6224f48bec77848dcb0
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-16 20:09:48 +0200

    Refactor tool-anchor template to include flex and gap classes

--- internal/web/shared/components/tool-anchor_templ.go
+++ internal/web/shared/components/tool-anchor_templ.go
@@ -71,7 +71,7 @@ func ToolAnchor(tool *models.Tool, options *ToolAnchorOptions) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</span> <span>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</span> <span class=\"flex gap\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
