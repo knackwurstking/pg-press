a48b6b3fc75ec15f3c05ae46639eeeae4da874ec
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-24 07:37:25 +0200

    Do nil checks for error methods and fix error formatting (%w -> %v)

--- services/base.go
+++ services/base.go
@@ -23,7 +23,7 @@ func NewBase(r *Registry, l *logger.Logger) *Base {
 
 func (b *Base) CreateTable(query, tableName string) error {
 	if _, err := b.DB.Exec(query); err != nil {
-		return fmt.Errorf("failed to create %s table: %w", tableName, err)
+		return fmt.Errorf("failed to create %s table: %v", tableName, err)
 	}
 	return nil
 }
@@ -37,17 +37,33 @@ func (b *Base) QueryCount(query string, args ...any) (int, error) {
 }
 
 func (b *Base) GetSelectError(err error) error {
-	return fmt.Errorf("database select failed: %w", err)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("database select failed: %v", err)
 }
 
 func (b *Base) GetInsertError(err error) error {
-	return fmt.Errorf("database insert failed: %w", err)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("database insert failed: %v", err)
 }
 
 func (b *Base) GetUpdateError(err error) error {
-	return fmt.Errorf("database update failed: %w", err)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("database update failed: %v", err)
 }
 
 func (b *Base) GetDeleteError(err error) error {
-	return fmt.Errorf("database delete failed: %w", err)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("database delete failed: %v", err)
 }
