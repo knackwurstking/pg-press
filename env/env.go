15ebf1165bef5e5b5caf27f536edea9c932742d6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-02 13:51:41 +0100

    docs: update info

--- env/env.go
+++ env/env.go
@@ -4,6 +4,6 @@ import "os"
 
 var (
 	ServerPathPrefix = os.Getenv("SERVER_PATH_PREFIX")
-	LogLevel         = os.Getenv("LOG_LEVEL")  // LogLevel would be "debug", "info", "warn", "error", or "fatal" (case-insensitive)
+	LogLevel         = os.Getenv("LOG_LEVEL")  // LogLevel would be "debug", "info", "warn", "error", or "fatal"
 	LogFormat        = os.Getenv("LOG_FORMAT") // LogFormat would be "json" or "text"
 )
