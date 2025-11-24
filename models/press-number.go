1bcd03bccb8ebb270e2f9f1e0ae873e1034b9adf
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-24 21:29:00 +0100

    Add PressNumberUnknown constant for invalid press numbers
    Add a new constant PressNumberUnknown with value -1 to represent tools
    that can no longer be bound to a valid press. This provides a clear
    sentinel value for handling invalid press number cases in the system.

--- models/press-number.go
+++ models/press-number.go
@@ -2,6 +2,11 @@ package models
 
 import "slices"
 
+const (
+	// PressNumberUnknown is used for tools i can no longer bind to a valid press
+	PressNumberUnknown PressNumber = -1
+)
+
 type PressNumber int8
 
 func IsValidPressNumber(n *PressNumber) bool {
