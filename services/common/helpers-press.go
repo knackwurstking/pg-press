556f261960ed6f1aebe8373d407c569195528e51
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-11 20:35:27 +0100

    Refactor shit, cleanup...

--- services/common/helpers-press.go
+++ services/common/helpers-press.go
@@ -4,23 +4,27 @@ import (
 	"github.com/knackwurstking/pg-press/services/shared"
 )
 
+const (
+	SQLGetPressNumberForTool string = `
+		SELECT id, slot_up, slot_down FROM presses WHERE slot_up = :slot_up OR slot_down = :slot_down;
+	`
+)
+
 func GetPressNumberForTool(db DB, toolID shared.EntityID) (shared.PressNumber, shared.Slot, error) {
-	var pressNumber shared.PressNumber
+	var pressNumber shared.PressNumber = -1
 	var slotUp, slotDown shared.EntityID
 
-	err := db.Press.Press.DB().QueryRow(
-		"SELECT id, slot_up, slot_down FROM presses WHERE slot_up = ? OR slot_down = ?",
-		toolID, toolID,
-	).Scan(&pressNumber, &slotUp, &slotDown)
+	err := db.Press.Press.DB().QueryRow(SQLGetPressNumberForTool, toolID, toolID).Scan(&pressNumber, &slotUp, &slotDown)
 	if err != nil {
-		return -1, shared.SlotUnknown, err
+		return pressNumber, shared.SlotUnknown, err
 	}
 
-	if slotUp == toolID {
+	switch toolID {
+	case slotUp:
 		return pressNumber, shared.SlotUp, nil
-	}
-	if slotDown == toolID {
+	case slotDown:
 		return pressNumber, shared.SlotDown, nil
+	default:
+		return pressNumber, shared.SlotUnknown, nil
 	}
-	return pressNumber, shared.SlotUnknown, nil
 }
