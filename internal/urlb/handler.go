8fb3cb46e87c858aa778b25a6de72d5cc3b42f91
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-19 02:24:28 +0100

    refactor: add debug logs to urlb handler functions

--- internal/urlb/handler.go
+++ internal/urlb/handler.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/knackwurstking/pg-press/internal/errors"
+	"github.com/knackwurstking/pg-press/internal/logger"
 	"github.com/labstack/echo/v4"
 )
+
+var log = logger.New("urlb")
 
 // RedirectTo performs an HTTP redirect to the specified path
 func RedirectTo(c echo.Context, path templ.SafeURL) *errors.MasterError {
@@ -21,16 +24,19 @@ func RedirectTo(c echo.Context, path templ.SafeURL) *errors.MasterError {
 
 // SetHXTrigger sets HX-Trigger header
 func SetHXTrigger(c echo.Context, events ...string) {
+	log.Debug("Setting HX-Trigger to %s", strings.Join(events, ", "))
 	c.Response().Header().Set("HX-Trigger", strings.Join(events, ", "))
 }
 
 // SetHXRedirect sets HX-Redirect header
 func SetHXRedirect(c echo.Context, path templ.SafeURL) {
+	log.Debug("Setting HX-Redirect to %s", path)
 	c.Response().Header().Set("HX-Redirect", string(path))
 }
 
 // SetHXAfterSettle sets HX-Trigger-After-Settle header with JSON data
 func SetHXAfterSettle(c echo.Context, data map[string]any) {
 	triggerDataJSON, _ := json.Marshal(data)
+	log.Debug("Setting HX-Trigger-After-Settle to %s", string(triggerDataJSON))
 	c.Response().Header().Set("HX-Trigger-After-Settle", string(triggerDataJSON))
 }
