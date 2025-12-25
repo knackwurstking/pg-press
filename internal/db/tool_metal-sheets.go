79226c55449e77d3f26948c705e050196bf33add
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-25 19:26:23 +0100

    feat(db): add upper and lower metal sheet queries

--- internal/db/tool_metal-sheets.go
+++ internal/db/tool_metal-sheets.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"database/sql"
+
 	"github.com/knackwurstking/pg-press/internal/errors"
 	"github.com/knackwurstking/pg-press/internal/shared"
 )
@@ -16,17 +18,65 @@ const (
 			tool_id 		INTEGER NOT NULL,
 			tile_height 	REAL NOT NULL,
 			value 			REAL NOT NULL,
-			type 			TEXT NOT NULL,
+			type 			TEXT NOT NULL, 			-- 'upper' | 'lower'
 			marke_height 	INTEGER,
 			stf 			REAL,
 			stf_max 		REAL,
-			identifier 		TEXT,
+			identifier 		TEXT, 					-- e.g. 'SACMI' | 'SITI'
 
 			PRIMARY KEY("id" AUTOINCREMENT),
 			FOREIGN KEY(tool_id) REFERENCES tools(id) ON DELETE CASCADE
 		);
 	`
+
+	sqlListUpperMetalSheetsByTool string = `
+		SELECT id, tool_id, tile_height, value
+		FROM metal_sheets
+		WHERE tool_id = :tool_id AND type = 'upper';
+	`
+
+	sqlListLowerMetalSheetsByTool string = `
+		SELECT id, tool_id, tile_height, value, marke_height, stf, stf_max, identifier
+		FROM metal_sheets
+		WHERE tool_id = :tool_id AND type = 'lower';
+	`
 )
+
+func ListUpperMetalSheetsByTool(toolID shared.EntityID) ([]*shared.UpperMetalSheet, *errors.MasterError) {
+	rows, err := dbTool.Query(sqlListUpperMetalSheetsByTool, sql.Named("tool_id", int64(toolID)))
+	if err != nil {
+		return nil, errors.NewMasterError(err)
+	}
+	defer rows.Close()
+
+	var metalSheets []*shared.UpperMetalSheet
+	for rows.Next() {
+		ums, merr := ScanUpperMetalSheet(rows)
+		if merr != nil {
+			return nil, merr.Wrap("could not scan upper metal sheet")
+		}
+		metalSheets = append(metalSheets, ums)
+	}
+	return metalSheets, nil
+}
+
+func ListLowerMetalSheetsByTool(toolID shared.EntityID) ([]*shared.LowerMetalSheet, *errors.MasterError) {
+	rows, err := dbTool.Query(sqlListLowerMetalSheetsByTool, sql.Named("tool_id", int64(toolID)))
+	if err != nil {
+		return nil, errors.NewMasterError(err)
+	}
+	defer rows.Close()
+
+	var metalSheets []*shared.LowerMetalSheet
+	for rows.Next() {
+		lms, merr := ScanLowerMetalSheet(rows)
+		if merr != nil {
+			return nil, merr.Wrap("could not scan lower metal sheet")
+		}
+		metalSheets = append(metalSheets, lms)
+	}
+	return metalSheets, nil
+}
 
 // -----------------------------------------------------------------------------
 // Scan Helpers
