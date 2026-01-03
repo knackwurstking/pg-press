f8a29e94c6b4eb79e741dd2d2634cc2a1525502d
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-03 08:57:45 +0100

    feat(dialogs): add edit press handler functions and data structures [WIP]

--- internal/handlers/dialogs/edit-press.go
+++ internal/handlers/dialogs/edit-press.go
@@ -1,1 +1,21 @@
 package dialogs
+
+import (
+	"github.com/knackwurstking/pg-press/internal/errors"
+	"github.com/labstack/echo/v4"
+)
+
+func GetEditPress(c echo.Context) *echo.HTTPError {
+}
+
+func PostPress(c echo.Context) *echo.HTTPError {
+}
+
+func PutPress(c echo.Context) *echo.HTTPError {
+}
+
+type EditPressFormData struct {
+}
+
+func GetEditPressFormData() (*EditPressFormData, *errors.MasterError) {
+}
