5d451dedfad33334fba2f967ccbbb830566ef342
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-05 16:37:43 +0100

    feat(handlers/press): Add delete press handler

--- internal/handlers/press/delete.go
+++ internal/handlers/press/delete.go
@@ -1,7 +1,25 @@
 package press
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/knackwurstking/pg-press/internal/db"
+	"github.com/knackwurstking/pg-press/internal/shared"
+	"github.com/knackwurstking/pg-press/internal/urlb"
+	"github.com/labstack/echo/v4"
+)
 
 func DeletePress(c echo.Context) *echo.HTTPError {
-	// TODO: Remove this press and redirect to the tools page
+	var pressNumber shared.PressNumber
+	if pn, merr := shared.ParseParamInt8(c, "press"); merr != nil {
+		return merr.Echo()
+	} else {
+		pressNumber = shared.PressNumber(pn)
+	}
+
+	if merr := db.DeletePress(pressNumber); merr != nil {
+		return merr.WrapEcho("delete press %d", pressNumber)
+	}
+
+	urlb.SetHXRedirect(c, urlb.UrlPress(pressNumber).Page)
+
+	return nil
 }
