1fcdac44154ede27eef328f9736587d2a2a1d87c
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-14 17:30:24 +0100

    feat(troublereports): remove handler struct and methods

--- internal/handlers/troublereports/troublereports.go
+++ internal/handlers/troublereports/troublereports.go
@@ -8,15 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Handler struct {
-	// No db field needed as we'll use global db instances
-}
-
-func NewHandler() *Handler {
-	return &Handler{}
-}
-
-func (h *Handler) GetPage(c echo.Context) *echo.HTTPError {
+func GetPage(c echo.Context) *echo.HTTPError {
 	t := templates.Page()
 	err := t.Render(c.Request().Context(), c.Response())
 	if err != nil {
@@ -26,37 +18,37 @@ func (h *Handler) GetPage(c echo.Context) *echo.HTTPError {
 	return nil
 }
 
-func (h *Handler) GetSharePDF(c echo.Context) *echo.HTTPError {
+func GetSharePDF(c echo.Context) *echo.HTTPError {
 	// This would normally generate a PDF, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "PDF generation not implemented")
 }
 
-func (h *Handler) GetAttachment(c echo.Context) *echo.HTTPError {
+func GetAttachment(c echo.Context) *echo.HTTPError {
 	// This would normally serve an attachment, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "Attachment serving not implemented")
 }
 
-func (h *Handler) GetModificationsForID(c echo.Context) *echo.HTTPError {
+func GetModificationsForID(c echo.Context) *echo.HTTPError {
 	// This would normally show modifications, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "Modifications page not implemented")
 }
 
-func (h *Handler) HTMXGetData(c echo.Context) *echo.HTTPError {
+func HTMXGetData(c echo.Context) *echo.HTTPError {
 	// This would normally return HTMX data, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "HTMX data not implemented")
 }
 
-func (h *Handler) HTMXDeleteTroubleReport(c echo.Context) *echo.HTTPError {
+func HTMXDeleteTroubleReport(c echo.Context) *echo.HTTPError {
 	// This would normally delete a trouble report, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "Delete functionality not implemented")
 }
 
-func (h *Handler) HTMXGetAttachmentsPreview(c echo.Context) *echo.HTTPError {
+func HTMXGetAttachmentsPreview(c echo.Context) *echo.HTTPError {
 	// This would normally preview attachments, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "Attachments preview not implemented")
 }
 
-func (h *Handler) HTMXPostRollback(c echo.Context) *echo.HTTPError {
+func HTMXPostRollback(c echo.Context) *echo.HTTPError {
 	// This would normally rollback a trouble report, but for now return not implemented
 	return echo.NewHTTPError(http.StatusNotImplemented, "Rollback functionality not implemented")
 }
