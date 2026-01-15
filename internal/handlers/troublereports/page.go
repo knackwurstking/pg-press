407ffe6c5276fce5ee1fccf86d57396750f72b79
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-15 14:48:27 +0100

    refactor(troublereports): simplify error handling in page rendering

--- internal/handlers/troublereports/page.go
+++ internal/handlers/troublereports/page.go
@@ -1,17 +1,15 @@
 package troublereports
 
 import (
-	"net/http"
-
+	"github.com/knackwurstking/pg-press/internal/errors"
 	"github.com/knackwurstking/pg-press/internal/handlers/troublereports/templates"
 	"github.com/labstack/echo/v4"
 )
 
 func GetPage(c echo.Context) *echo.HTTPError {
 	t := templates.Page()
-	err := t.Render(c.Request().Context(), c.Response())
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to render page")
+	if err := t.Render(c.Request().Context(), c.Response()); err != nil {
+		return errors.NewRenderError(err, "Page")
 	}
 
 	return nil
