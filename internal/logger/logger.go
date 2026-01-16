b1533de8367f9a7e0195a4f31fd1b2a86f9e6201
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-16 13:04:00 +0100

    Add verbose logging option to logger initialization

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,9 +1,13 @@
 package logger
 
 import (
+	"github.com/knackwurstking/pg-press/internal/env"
 	"github.com/knackwurstking/ui/ui-templ"
 )
 
 func New(group string) *ui.Logger {
+	if env.Verbose {
+		return ui.NewLoggerWithVerbose(group)
+	}
 	return ui.NewLogger(group)
 }
