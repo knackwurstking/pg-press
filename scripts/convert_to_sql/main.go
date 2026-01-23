02433ca998f701a7a1b3d7731ec1efb5fd993ca9
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-23 16:36:24 +0100

    Add SQL conversion scripts and update Makefile

---
+++ scripts/convert_to_sql/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// TODO: convert "./images" and "./json" to new SQL database format "..."
+	// 	- "sql/tool.sqlite"
+	//  - "sql/press.sqlite"
+	//  - "sql/note.sqlite"
+	//  - "sql/user.sqlite"
+	//  - "sql/reports.sqlite"
+}
