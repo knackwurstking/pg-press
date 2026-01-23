5ed0d1157cbc91f67379c1e1b97b681c57e38f44
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-23 19:53:46 +0100

    Convert data to SQL database format with placeholder functions

--- scripts/convert_to_sql/main.go
+++ scripts/convert_to_sql/main.go
@@ -1,10 +1,56 @@
 package main
 
+import "errors"
+
+//import j "github.com/knackwurstking/pg-press/scripts/convert_to_json"
+
 func main() {
 	// TODO: convert "./images" and "./json" to new SQL database format "..."
-	// 	- "sql/tool.sqlite"
-	//  - "sql/press.sqlite"
-	//  - "sql/note.sqlite"
-	//  - "sql/user.sqlite"
-	//  - "sql/reports.sqlite"
+	// 	- "sql/tool.sqlite"	    : metal_sheets, tool_regenerations, tools
+	//  - "sql/press.sqlite"    : cycles, presses, press_regenerations
+	//  - "sql/note.sqlite"	    : notes
+	//  - "sql/user.sqlite"	    : cookies, users
+	//  - "sql/reports.sqlite"	: trouble_reports
+
+	var err error
+
+	if err = toolData(); err != nil {
+		panic("failed to convert tool data: " + err.Error())
+	}
+
+	if err = pressData(); err != nil {
+		panic("failed to convert press data: " + err.Error())
+	}
+
+	if err = noteData(); err != nil {
+		panic("failed to convert note data: " + err.Error())
+	}
+
+	if err = userData(); err != nil {
+		panic("failed to convert user data: " + err.Error())
+	}
+
+	if err = reportsData(); err != nil {
+		panic("failed to convert reports data: " + err.Error())
+	}
+}
+
+func toolData() error {
+	return errors.New("not implemented yet")
+}
+
+func pressData() error {
+	return errors.New("not implemented yet")
+}
+
+func noteData() error {
+	return errors.New("not implemented yet")
+}
+
+func userData() error {
+	return errors.New("not implemented yet")
+}
+
+func reportsData() error {
+	return errors.New("not implemented yet")
 }
