bbebecb6c41d8008ce935d9e7154121dc93b57dd
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-08 06:36:44 +0100

    Fix missing press number field in database update

--- internal/handlers/dialogs/edit-press.go
+++ internal/handlers/dialogs/edit-press.go
@@ -72,6 +72,7 @@ func PutPress(c echo.Context) *echo.HTTPError {
 
 	merr := db.UpdatePress(&shared.Press{
 		ID:           press.ID,
+		Number:       press.Number,
 		Type:         data.MachineType,
 		Code:         data.Code,
 		CyclesOffset: data.CyclesOffset,
