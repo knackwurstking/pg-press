f4d9bde4a43848d79baa77b209df87d5f67d57dc
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-25 14:56:31 +0100

    Migrate to new tabs implementation and remove obsolete code
    - Use new templui tabs component for tool/press switching
    - Replace scripts.templ with inline script in page template
    - Remove user parameter from Page() (no longer needed)
    - Add spinner components for tab content loading
    - Fix hx-target IDs to match new tab content structure
    - Clean up CSS output (remove unused tailwind transform classes)

--- internal/handlers/tools/page.go
+++ internal/handlers/tools/page.go
@@ -3,18 +3,12 @@ package tools
 import (
 	"github.com/knackwurstking/pg-press/internal/errors"
 	"github.com/knackwurstking/pg-press/internal/handlers/tools/templates"
-	"github.com/knackwurstking/pg-press/internal/utils"
 
 	"github.com/labstack/echo/v4"
 )
 
 func GetToolsPage(c echo.Context) *echo.HTTPError {
-	user, merr := utils.GetUserFromContext(c)
-	if merr != nil {
-		return merr.Echo()
-	}
-
-	t := templates.Page(templates.PageProps{User: user})
+	t := templates.Page()
 	err := t.Render(c.Request().Context(), c.Response())
 	if err != nil {
 		return errors.NewRenderError(err, "Tools Page")
